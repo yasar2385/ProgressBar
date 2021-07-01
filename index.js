dca5fdf69627338c109895ff8cdd1a7a875322d6
yasar2385 <yasar.mohideen@newgen.co>
2021-07-02 01:04:06 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function myKeyPress(e) {
+  var keynum;
+
+  if (window.event) {
+    // IE
+    keynum = e.keyCode;
+  } else if (e.which) {
+    // Netscape/Firefox/Opera
+    keynum = e.which;
+  }
+
+  alert(String.fromCharCode(keynum));
+}
